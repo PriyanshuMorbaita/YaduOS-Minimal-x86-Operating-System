c8814166ca71ff9ee49bf513169c752c03519020
Priyanshu Morbaita <129761304+PriyanshuMorbaita@users.noreply.github.com>
2025-08-15 22:53:08 +0530

    Add files via upload

---
+++ YaduOS/programs/stdlib/src/peachos.c
@@ -0,0 +1,86 @@
+#include "peachos.h"
+#include "string.h"
+
+struct command_argument* peachos_parse_command(const char* command, int max){
+    struct command_argument* root_command;
+    char scommand[1025];
+    if(max >= (int)sizeof(scommand)){
+        return 0;
+    }
+
+    strncpy(scommand,command,sizeof(scommand));
+    char* token = strtok(scommand," ");
+    if(!token){
+        goto out;
+    }
+
+    root_command = peachos_malloc(sizeof(struct command_argument));
+    if(!root_command){
+        goto out;
+    }
+
+    strncpy(root_command->argument,token,sizeof(root_command->argument));
+    root_command->next = 0;
+
+    struct command_argument* current = root_command;
+    token = strtok(NULL," ");
+    while(token != 0){
+        struct command_argument* new_command = peachos_malloc(sizeof(struct command_argument));
+        if(!new_command){
+            break;
+        }
+
+        strncpy(new_command->argument,token,sizeof(new_command->argument));
+        new_command->next = 0x00;
+        current->next = new_command;
+        current = new_command;
+        token = strtok(NULL," ");
+    }
+
+out:
+    return root_command;
+}
+
+int peachos_getkeyblock(){
+    int val = 0;
+    do{
+        val = peachos_getkey();
+    }while(val == 0);
+    return val;
+}
+
+void peachos_terminal_readline(char* out, int max, bool output_while_typing){
+    int i=0; 
+    for(i=0; i<max-1; i++){
+        char key = peachos_getkeyblock();
+
+        if(key == 13){
+            break;
+        }
+
+        if(output_while_typing){
+            peachos_putchar(key);
+        }
+
+        if(key == 0x08 && i>=1){
+            out[i-1] = 0x00;
+            i -= 2;
+            continue;
+        }
+
+        out[i] = key;
+    }
+
+    out[i] = 0x00;
+}
+
+int peachos_system_run(const char* command){
+    char buffer[1024];
+    strncpy(buffer,command,sizeof(buffer));
+    struct command_argument* root_command_argument = peachos_parse_command(buffer,sizeof(buffer));
+    if(!root_command_argument){
+        return -1;
+    }
+
+    return peachos_system(root_command_argument);
+}
---
+++ YaduOS/programs/stdlib/src/string.h
@@ -0,0 +1,22 @@
+#ifndef PEACHOS_STRING_H
+#define PEACHOS_STRING_H
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+
+size_t strlen(const char*);
+bool isdigit(char c);
+int toNumericDigit(char);
+void* strcpy(char* dest, const char* src);
+size_t strnlen(const char*,size_t);
+int strcmp(const char* str1, const char* str2);
+int strncmp(const char* st1, const char* str2, size_t len);
+char tolower(char ch);
+int istrcmp(const char* str1, const char* str2);
+int istrncmp(const char* str1, const char* str2, size_t len);
+int strnlen_terminator(const char* str, int max, char terminator);
+void* strncpy(char* dest, const char* src, size_t maxlen);
+char* strtok(char* str, const char* delimiters);
+
+#endif
